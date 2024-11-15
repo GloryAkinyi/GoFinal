256b19d5bd815d95a9faab527e080b66a9c8a8d3
gloryakinyi <akinyiglory2@gmail.com>
2024-11-15 11:40:34 +0300

    Initial Commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('service/', views.service, name='service'),
     path('starter/', views.starter, name='starter'),
+    path('about/', views.about, name='about'),
+    path('doctors/', views.doctors, name='doctors'),
+    path('myservice/', views.myservice, name='myservice'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('show/', views.show, name='show'),
+    path('delete/<int:id>', views.delete),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from myapp.forms import AppointmentForm
+from myapp.models import Appointment
 
 # Create your views here.
 def index(request):
@@ -9,3 +12,49 @@ def service(request):
 
 def starter(request):
     return render(request,'starter-page.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def doctors(request):
+    return render(request,'doctors.html')
+
+def myservice(request):
+    return render(request,'services.html')
+
+def appointment(request):
+    if request.method == 'POST':
+        myappointments=Appointment(
+            name = request.POST['name'],
+            email = request.POST['email'],
+            phone = request.POST['phone'],
+            datetime = request.POST['date'],
+            department = request.POST['department'],
+            doctor= request.POST['doctor'],
+            message = request.POST['message']
+        )
+        myappointments.save()
+        return redirect('/appointment')
+
+    else:
+        return render(request,'appointment.html')
+
+def show(request):
+    allappointments=Appointment.objects.all()
+    return render(request,'show.html',{'appointment':allappointments})
+
+def delete(request,id):
+    appoint = Appointment.objects.get(id = id)
+    appoint.delete()
+    return redirect('/show')
+
+def edit(request, id):
+    item = get_object_or_404(Appointment, id=id)
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('/show')
+    else:
+        form = AppointmentForm(instance=item)
+    return render(request, 'edit.html', {'form': form})
